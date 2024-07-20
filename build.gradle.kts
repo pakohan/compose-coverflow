0a30b10c35d76496f34f7019cf0f84cb8aba642a
Patrick Kohan <patrick.kohan@gmail.com>
2024-07-20 21:48:52 +0700

    generate doc

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,14 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("org.jetbrains.dokka") version "1.9.20"
+    alias(libs.plugins.android.library) apply false
+}
+
+tasks.dokkaHtmlMultiModule {
+    outputDirectory.set(layout.buildDirectory.dir("doc"))
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
 }
