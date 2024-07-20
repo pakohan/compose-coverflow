bab37ea56e90a2bf394f4e0080df198072c38fb1
Patrick Kohan <patrick.kohan@gmail.com>
2024-07-20 21:52:34 +0700

    /

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     id("org.jetbrains.dokka") version "1.9.20"
-    alias(libs.plugins.android.library) apply false
 }
 
 tasks.dokkaHtmlMultiModule {
