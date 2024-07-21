49c2818779299da552898c6f4d446cd1226fabd8
Patrick Kohan <patrick.kohan@gmail.com>
2024-07-21 09:48:17 +0700

    add some tests

---
+++ CoverFlow/src/test/java/com/pakohan/coverflow/OffsetLinearDistanceFactorTest.kt
@@ -0,0 +1,28 @@
+package com.pakohan.coverflow
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
+import org.junit.Test
+
+class OffsetLinearDistanceFactorTest {
+    @Test
+    fun factor_fromHalfToOne_success() {
+        val ldf = OffsetLinearDistanceFactor(
+            .5f,
+            1f
+        )
+        val cases = mapOf(
+            0f to 0f,
+            .5f to 0f,
+            .75f to .5f,
+            1f to 1f,
+        )
+
+        cases.forEach { (input, output) ->
+            assertEquals(
+                ldf.factor(input),
+                output
+            )
+        }
+    }
+}
