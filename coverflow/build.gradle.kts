b1dff172b2cc3a586c72f67801fdc6ba1331e560
Patrick Kohan <patrick.kohan@gmail.com>
2024-07-25 11:14:20 +0700

    first draft

--- coverflow/build.gradle.kts
+++ coverflow/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
     alias(libs.plugins.org.jetbrains.kotlin.plugin.compose)
     alias(libs.plugins.org.jetbrains.dokka)
     id("kotlin-parcelize")
+    id("maven-publish")
+    id("signing")
+    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
 }
 
 android {
@@ -43,6 +46,70 @@ android {
         }
     }
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "io.github.pakohan"
+            artifactId = "coverflow"
+            version = "1.0.0-SNAPSHOT"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+
+            pom {
+                name = "CoverFlow"
+                description = "A Jetpack Compose implementation of CoverFlow"
+                url = "https://github.com/pakohan/compose-coverflow"
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://github.com/pakohan/compose-coverflow/blob/main/LICENSE"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "pakohan"
+                        name = "Patrick Kohan"
+                        email = "patrick.kohan@gmail.com"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com:pakohan/compose-coverflow.git"
+                    developerConnection = "scm:git:ssh://github.com:pakohan/compose-coverflow.git"
+                    url = "https://github.com/pakohan/compose-coverflow"
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
+}
+
+signing {
+    val signingKey = providers
+        .environmentVariable("GPG_SIGNING_KEY")
+        .forUseAtConfigurationTime()
+    val signingPassphrase = providers
+        .environmentVariable("GPG_SIGNING_PASSPHRASE")
+        .forUseAtConfigurationTime()
+    if (signingKey.isPresent && signingPassphrase.isPresent) {
+        useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
+        val extension = extensions
+            .getByName("publishing") as PublishingExtension
+        sign(extension.publications)
+    }
+}
+
 
 dependencies {
     implementation(libs.androidx.core.ktx)
